ea936db3920e743e51caf92545832cda036a9a00
devrianmendes <dev.rianmendes@outlook.com>
2021-08-21 20:34:49 -0300

    Project 90% complete (not responsive YET)

---
+++ script.js
@@ -0,0 +1,12 @@
+var hamburguer = document.querySelector('.hamburguer');
+var container = document.querySelector('.container');
+
+
+
+hamburguer.addEventListener('click', clicar);
+
+function clicar(){
+    container.classList.toggle('show-menu');
+}
+
+
